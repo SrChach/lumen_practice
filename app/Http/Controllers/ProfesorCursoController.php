5c488e565d2c2462880544c1810ab32cf15ebbb5
srchach <gsikmo@gmail.com>
2019-03-29 03:07:51 -0600

    Editar recursos anidados

--- app/Http/Controllers/ProfesorCursoController.php
+++ app/Http/Controllers/ProfesorCursoController.php
@@ -15,15 +15,16 @@ class ProfesorCursoController extends Controller
 		public function index($profesor_id){
 			$profesor = Profesor::find($profesor_id);
 
-			if($profesor){
-				$cursos = $profesor->cursos;
-				return $this->responder($cursos, 200);
-			}
-			return $this->responder_error('No se pudo encontrar profesor con el id dado', 404);
+			if(!$profesor)
+				return $this->responder_error('No se pudo encontrar profesor con el id dado', 404);
+	
+			$cursos = $profesor->cursos;
+			return $this->responder($cursos, 200);
 		}
 
 		// Crea un curso, dicho curso asignado a un profesor (recibe $profesor_id)
 		public function store(Request $request, $profesor_id){
+
 			$profesor = Profesor::find($profesor_id);
 			if(!$profesor)
 				return $this->responder_error("El profesor especificado no existe", 404);
@@ -37,9 +38,28 @@ class ProfesorCursoController extends Controller
 			return $this->responder('Curso creado', 200);
 		}
 
-		public function update(){
-			return "desde 'update' en ProdesorCursoController";
+		public function update(Request $request, $profesor_id, $curso_id){
+			
+			$profesor = Profesor::find($profesor_id);
+			if(!$profesor)
+				return $this->responder_error("El profesor especificado no existe", 404);
+
+			$curso = Curso::find($curso_id);
+			if(!$curso)
+				return $this->responder_error("El curso especificado no existe", 404);
+
+			$this->validarCurso($request);
+
+			$curso->titulo = $request->get('titulo');		
+			$curso->descripcion = $request->get('descripcion');		
+			$curso->valor = $request->get('valor');		
+			$curso->profesor_id = $profesor_id;
+
+			$curso->save();
+
+			return $this->responder('Curso editado', 200);		
 		}
+
 		public function destroy(){
 			return "desde 'destroy' en ProdesorCursoController";
 		}
