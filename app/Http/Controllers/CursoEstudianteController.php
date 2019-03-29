46808cb1361f44658c7329cb01f41a78b8078c4f
srchach <gsikmo@gmail.com>
2019-03-28 18:40:26 -0600

    Listado de elementos dependientes de otros elementos

--- app/Http/Controllers/CursoEstudianteController.php
+++ app/Http/Controllers/CursoEstudianteController.php
@@ -2,15 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Curso;
+
 class CursoEstudianteController extends Controller
 {
 		public function __construct(){
 				//
 		}
 
-		public function index(){
-			return "desde 'index' en CursoEstudianteController";
+		public function index($curso_id){
+			$curso = Curso::find($curso_id);
+
+			if($curso){
+				$estudiantes = $curso->estudiantes;
+				return $this->responder($estudiantes, 200);
+			}
+			return $this->responder_error('No se pudo encontrar curso con el id dado', 404);
 		}
+		
 		public function store(){
 			return "desde 'store' en CursoEstudianteController";
 		}
--- app/Http/Controllers/ProfesorCursoController.php
+++ app/Http/Controllers/ProfesorCursoController.php
@@ -2,15 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Profesor;
+
 class ProfesorCursoController extends Controller
 {
 		public function __construct(){
 				//
 		}
 
-		public function index(){
-			return "desde 'index' en ProdesorCursoController";
+		public function index($profesor_id){
+			$profesor = Profesor::find($profesor_id);
+
+			if($profesor){
+				$cursos = $profesor->cursos;
+				return $this->responder($cursos, 200);
+			}
+			return $this->responder_error('No se pudo encontrar profesor con el id dado', 404);
 		}
+
 		public function store(){
 			return "desde 'store' en ProdesorCursoController";
 		}
